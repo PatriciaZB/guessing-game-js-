d72962e7d3ab56a7b349b7ed931a763ffc9cf2c4
Patricia Zegarra <patty.zegarrab@gmail.com>
2021-04-06 16:00:36 +0200

    loop

--- app.js
+++ app.js
@@ -2,3 +2,12 @@ const targetNum = Math.floor(Math.random() * maximun) + 1;
 
 let guess = parteInt(prompt("Enter your first guess!"));
 let attempts = 1;
+
+while (guess !== targetNum) {
+  attempts++;
+  if (guess > targetNum) {
+    guess = parteInt(prompt("Too high! Enter a new guess:"));
+  } else {
+    guess = parseInt(prompt("Too low! Enter a new guess:"))
+  }
+}
